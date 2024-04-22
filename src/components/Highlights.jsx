d33e9ad5c7b216da0ee590ce076ff2a6ef7c7fe3
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-22 03:29:19 +0100

    Started highlights section with heading text

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Highlights = () => {
   return (
-    <div>
-      This is the highlight
-    </div>
+    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights
+          </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
