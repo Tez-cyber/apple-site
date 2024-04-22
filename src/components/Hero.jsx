e979bca6d820c713f59ae49c173f1a1b68923f13
Salami Azeez <salamitopeazeez@gmail.com>
2024-04-22 03:20:15 +0100

    Added cta section with animation without video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,12 @@ const Hero = () => {
       opacity: 1,
       delay: 1.5
     })
+
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2.5
+    })
   }, [])
   return (
     <section className='w-full nav-height bg-black relative'>
@@ -34,6 +40,10 @@ const Hero = () => {
                 <source src={videoSrc} type='video/mp4' />
               </video>
             </div>
+        </div>
+        <div id="cta" className='flex flex-col items-center opacity-0 translate-y-20'>
+          <a href="#highlights" className='btn'>Buy</a>
+          <p>From $199/month or $999</p>
         </div>
     </section>
   )
