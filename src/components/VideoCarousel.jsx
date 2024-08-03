cf6913febca6ac1c5f66a3cd61f1a8b85fb188a0
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-03 11:21:14 +0100

    Mapped highlightsSlides array to add videos

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { highlightsSlides } from '../constants'
 
 const VideoCarousel = () => {
   return (
-    <div>VideoCarousel</div>
+    <>
+        <div className="flex items-center">
+            {
+                highlightsSlides.map((list, i) => (
+                    <div key={i} id='slider' className='pr-10 sm:pr-20'>
+                        <div className='video-carousel_container'>
+                            <div className='w-full h-full text-center rounded-3xl overflow-hidden bg-black'>
+                                <video id='video' autoPlay playsInline={true} muted preload='auto' >
+                                    <source src={list.video} type='video/mp4' />
+                                </video>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    </>
   )
 }
 
