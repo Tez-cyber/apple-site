f03903471188439096d50153195710f76ca1747a
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-03 11:07:43 +0100

    Added links to highlights heading

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 import { useGSAP } from "@gsap/react"
 import gsap from 'gsap'
+import { watchImg, rightImg } from '../utils'
+
+
 const Highlights = () => {
+  //======= useGSAP
   useGSAP(() => {
     gsap.to('#title', {
       opacity: 1,
       y: 0
     })
+
+  //------
+  gsap.to(".link", {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    stagger: .25
+  })
   }, [])
   useGSAP
   return (
     <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
       <div className="screen-max-width">
-        <div>
+        <div className='mb-12 w-full items-end justify-between md:flex'>
           <h1 id="title" className="section-heading">
-            Get the highlights
+            Get the highlights.
           </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="film" className='ml-2' />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="event" className='ml-2' />
+            </p>
+          </div>
         </div>
       </div>
     </section>
