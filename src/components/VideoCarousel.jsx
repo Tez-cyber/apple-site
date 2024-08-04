40bfce4d13c7b383d9d559a1c2574a485bbd4b0d
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-04 05:12:46 +0100

    Added button to perform action on video carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { highlightsSlides } from '../constants'
+import gsap from 'gsap'
+import { pauseImg, playImg, replayImg } from '../utils'
+
 
 const VideoCarousel = () => {
     //======== Refs
@@ -8,34 +11,33 @@ const VideoCarousel = () => {
     const videoDivRef = useRef([])
 
     // ============ useStates
-    const [video, setSetVideo] = useState({
+    const [video, setVideo] = useState({
         isEnd: false,
         startPlay: false,
         videoId: 0,
         isLastVideo: false,
         isPlaying: false,
     })
-
     const [loadedData, setLoadedData] = useState([])
 
     const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video
 
     // ============ useEffects
     useEffect(() => {
-      if(loadedData.length > 3) {
-        if(!isPlaying) {
-            videoRef.current[videoId].pause()
-        } else {
-            startPlay && videoRef.current[videoId].play()
+        if (loadedData.length > 3) {
+            if (!isPlaying) {
+                videoRef.current[videoId].pause()
+            } else {
+                startPlay && videoRef.current[videoId].play()
+            }
         }
-      }
     }, [startPlay, videoId, isPlaying, loadedData])
-    
+
     useEffect(() => {
         const currentProgress = 0
         let span = videoSpanRef.current;
 
-        if(span[videoId]) {
+        if (span[videoId]) {
             // animate progress of the video
             let anim = gsap.to(span[videoId], {
                 onUpdate: () => {
@@ -48,42 +50,63 @@ const VideoCarousel = () => {
             })
         }
     }, [videoId, startPlay])
-  return (
-    <>
-        <div className="flex items-center">
-            {
-                highlightsSlides.map((list, i) => (
-                    <div key={i} id='slider' className='pr-10 sm:pr-20'>
-                        <div className='video-carousel_container'>
-                            <div className='w-full h-full text-center rounded-3xl overflow-hidden bg-black'>
-                                <video id='video' 
-                                    playsInline={true} 
-                                    muted
-                                    preload='auto'
-                                    ref={(el) => (videoRef.current[i] = el)}
-                                    onPlay= {() => {
-                                        setVideo((prevVideo) => ({...prevVideo, isPlaying: true}))
-                                    }}
-                                >
-                                    <source src={list.video} type='video/mp4' />
-                                </video>
-                            </div>
-                            <div className="absolute top-12 left-[5%] z-10">
-                                {
-                                    list.textLists.map(text => (
-                                        <p className="text-xl font-medium md:text-2xl" key={text}>
-                                            {text}
-                                        </p>
-                                    ))
-                                }
+    return (
+        <>
+            <div className="flex items-center">
+                {
+                    highlightsSlides.map((list, i) => (
+                        <div key={i} id='slider' className='pr-10 sm:pr-20'>
+                            <div className='video-carousel_container'>
+                                <div className='w-full h-full text-center rounded-3xl overflow-hidden bg-black'>
+                                    <video id='video'
+                                        playsInline={true}
+                                        muted
+                                        preload='auto'
+                                        ref={(el) => (videoRef.current[i] = el)}
+                                        onPlay={() => {
+                                            setVideo((prevVideo) => ({ ...prevVideo, isPlaying: true }))
+                                        }}
+                                    >
+                                        <source src={list.video} type='video/mp4' />
+                                    </video>
+                                </div>
+                                <div className="absolute top-12 left-[5%] z-10">
+                                    {
+                                        list.textLists.map(text => (
+                                            <p className="text-xl font-medium md:text-2xl" key={text}>
+                                                {text}
+                                            </p>
+                                        ))
+                                    }
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
-        </div>
-    </>
-  )
+                    ))
+                }
+            </div>
+            <div className="relative flex-center mt-10">
+                <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+                    {videoRef.current.map((_, i) => (
+                        <span
+                            key={i}
+                            className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+                            ref={(el) => (videoDivRef.current[i] = el)}
+                        >
+                            <span
+                                className="absolute h-full w-full rounded-full"
+                                ref={(el) => (videoSpanRef.current[i] = el)}
+                            />
+                        </span>
+                    ))}
+                </div>
+                <button className="control-btn">
+                    <img 
+                        src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg} 
+                        alt={isLastVideo ? 'replay' : !isPlaying ? 'play' : 'pause'} />
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default VideoCarousel
